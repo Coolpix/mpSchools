dbedd4aec12d447fab1f0821331ba74c58e3ff21
sergio alonso <sergio.alonso@atmira.com>
2018-04-26 17:50:13 +0200

    List students by letter

--- src/pages/listStudents/listStudents.ts
+++ src/pages/listStudents/listStudents.ts
@@ -14,14 +14,20 @@ import {HttpClient} from "@angular/common/http";
   templateUrl: 'listStudents.html',
 })
 export class ListStudentsPage {
-  students: Student[];
+  students: any;
+  arrayOfLetters: String[] = [];
 
   constructor(public navCtrl: NavController, private http: HttpClient) {
     this.http.get<Student[]>('http://clases-mp.eu-west-2.elasticbeanstalk.com/students').subscribe(
       result => {
-        debugger;
-        this.students = result.sort(function(a, b) {
-          return a.name.toUpperCase().trim().split('')[0] > b.name.toUpperCase().trim().split('')[0];
+        this.students = result.sort(function(a,b) {
+          if (a.name.toUpperCase().trim().split('')[0] > b.name.toUpperCase().trim().split('')[0]) {
+            return 1;
+          }
+          if (a.name.toUpperCase().trim().split('')[0] < b.name.toUpperCase().trim().split('')[0]) {
+            return -1;
+          }
+          return 0;
         });
         this.createArrayLetters(this.students);
       });
@@ -33,21 +39,28 @@ export class ListStudentsPage {
 
   createArrayLetters(students: Student[]){
     let arrayOfNames = [];
+    let arrayOfLetterName = [];
     for (let i=0; i<students.length; i++){
-      let arrayOfLetterName = [];
       if (arrayOfLetterName.length === 0){
-        arrayOfLetterName.push(students[i].name);
+        arrayOfLetterName.push(students[i].name.trim());
+        this.arrayOfLetters.push(students[i].name.toUpperCase().trim().split('')[0]);
       }else{
         let letterToCheck = students[i].name.toUpperCase().trim().split('')[0];
         if (letterToCheck === students[i-1].name.toUpperCase().trim().split('')[0]){
-          arrayOfLetterName.push(students[i].name)
+          arrayOfLetterName.push(students[i].name.trim())
         }else{
           arrayOfNames.push(arrayOfLetterName);
           arrayOfLetterName = [];
-          arrayOfLetterName.push(students[i].name);
+          arrayOfLetterName.push(students[i].name.trim());
+          this.arrayOfLetters.push(students[i].name.toUpperCase().trim().split('')[0]);
+        }
+        if (i === students.length -1){
+          arrayOfNames.push(arrayOfLetterName);
         }
       }
     }
+    this.students = arrayOfNames;
     console.log(arrayOfNames);
+    console.log(this.arrayOfLetters);
   }
 }
